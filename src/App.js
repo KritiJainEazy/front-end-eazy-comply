4c5e62777223472738eae5b570f9dda32f4c6b53
KritiJainEazy <kriti@eazycomply.in>
2023-12-04 15:54:55 +0530

    reconfigured pagelayout

--- src/App.js
+++ src/App.js
@@ -12,25 +12,35 @@ import { NotFoundPage } from "./components/pages/FrameworkPages/NotFoundPage";
 import { UserPage } from "./components/organisms/UserPage";
 import { AddUser } from "./components/organisms/AddUser";
 import { UsersIndexPage } from "./components/pages/FrameworkPages/UsersIndexPage";
+import { LoginPage } from "./components/pages/FrameworkPages/LoginPage";
 // UsersIndexPage";
 
 export const App = () => {
   return (
     // basename="/eazy-compliance-and-audits"
     <Router>
-      <PageLayout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/user" />} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={NAV_CONFIG?.NAV_LOGIN_LANDING_PAGE} />}
+        />
 
-          <Route path={NAV_CONFIG?.NAV_USER_PAGE} element={<UsersIndexPage />}>
-            <Route index element={<UserPage />} />
-            <Route path={NAV_CONFIG?.NAV_ADD_USER} element={<AddUser />} />
-          </Route>
+        <Route
+          path={NAV_CONFIG?.NAV_USER_PAGE}
+          element={
+            <PageLayout>
+              <UsersIndexPage />
+            </PageLayout>
+          }
+        >
           <Route index element={<UserPage />} />
-          {/* <Route path="contact" element={<Contact />} />*/}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </PageLayout>
+          <Route path={NAV_CONFIG?.NAV_ADD_USER} element={<AddUser />} />
+        </Route>
+        {/* To be removed, below line */}
+        <Route index element={<LoginPage />} />
+        {/* <Route path="contact" element={<Contact />} />*/}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </Router>
   );
 };
